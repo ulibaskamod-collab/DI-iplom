aa7fc01267335b692e5b0907e0dd7c890ef39c22
ulibaskamod-collab <ulibaskamod@gmail.com>
2026-05-18 15:50:57 +0300

    Update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,16 +1,25 @@
 import 'next-auth'
 
 declare module 'next-auth' {
-  interface Session {
-    user: {
-      id: string
-      name?: string | null
-      email?: string | null
-      image?: string | null
+    interface Session {
+        user: {
+            id: string
+            name?: string | null
+            email?: string | null
+            image?: string | null
+            role?: string | null  // ДОБАВИТЬ ЭТУ СТРОКУ
+        }
     }
-  }
 
-  interface User {
-    id: string
-  }
+    interface User {
+        id: string
+        role?: string | null  // ДОБАВИТЬ ЭТУ СТРОКУ
+    }
+}
+
+declare module 'next-auth/jwt' {
+    interface JWT {
+        id: string
+        role?: string | null  // ДОБАВИТЬ ЭТУ СТРОКУ
+    }
 }
